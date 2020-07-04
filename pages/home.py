f6e970c2241f8761588c942ce76a11fd247f9928
JC <acalizv@gmail.com>
2020-07-04 13:24:41 -0500

    Adding logos and Icons

--- pages/home.py
+++ pages/home.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -5,47 +6,26 @@ from server import app
 from templates.includes.generate_template import full_template
 import pandas as pd
 import plotly.express as px
+from server import db
 
-df = px.data.gapminder().query("country=='Canada'")
-fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+data = pd.read_sql(
+    '''
+    SELECT 
+        Date_part('year', fecha) as year,
+        count(*) as total_count
+    FROM violencia_intrafamiliar 
+    GROUP BY date_part('year', fecha)'''
+    , db.engine)
+fig = px.line(data_frame=data, x='year', y='total_count')
+fig.update_layout(title='Total Count of Intrafamily Violence')
 
 app.index_string = full_template
 
 graph = dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
+    id='violencia-graph',
+    figure = fig
+)
 
 layout = html.Div([
-    html.H3('App 1'),
-    dcc.Dropdown(
-        id='app-1-dropdown',
-        options=[
-            {'label': 'App 1 - {}'.format(i), 'value': i} for i in [
-                'NYC', 'MTL', 'LA'
-            ]
-        ]
-    ),
-    html.Div(id='app-1-display-value'),
-    dcc.Link('Go to App 2', href='/apps/app2'),
     graph
-
 ])
-
-
-
-
-
-@app.callback(
-    Output('app-1-display-value', 'children'),
-    [Input('app-1-dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
